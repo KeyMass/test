bce7287e6ad83e5b4f4791af72761bda885dfb4b
KeyMass <paraskevopoulos.makis@gmail.com>
2022-12-20 22:04:38 +0200

    test commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from functions import test
+def print_hi(name):
+    test()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
